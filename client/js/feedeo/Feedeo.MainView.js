30854d7ece6744fa5f672bdf4f1632cce45c870c
florian <florian@f5271d98-ee2f-48df-821e-fe2ef29e0807>
2009-03-09 08:22:35 +0000

    un peu de nettoyage dans la lib extjs
    git-svn-id: http://projet02.optionsi.net/svn/trunk@9 f5271d98-ee2f-48df-821e-fe2ef29e0807

--- client/js/feedeo/Feedeo.MainView.js
+++ client/js/feedeo/Feedeo.MainView.js
@@ -45,24 +45,29 @@ Feedeo.MainView = Ext.extend(Ext.TabPanel, {
                 add:this.onTabAdd,
                 scope : this
             }
-        )
+        );
     }, // eo function initComponent
     onOpenInTab : function(article){
         console.debug('Event openintabclick captured, article:',article);
         //add a fake panel for debug purpose
-        if(article!=null)
+        if(article!==null)
         {
             var panel = this.add({xtype:'panel',title:'15 first cha...',article:article,html:'<object style="width:100%;height:100%;" data="'+article.data.url+'"></object>',closable:true});
             //this.setActiveTab(panel);
             console.log(panel,this);
             this.doLayout(); //force le calcul du layout
-                             //et déclenche le préchargement de l'onglet grâce à deferredRender:false;
+                             //et décaenche le préchargement de l'onglet grâce à deferredRender:false;
         }
     },
     onTabRemove : function ()
     {
         //vérifier le nombre d'onglets et masquer si un seul onglet
         //this. hideTabStripItem(0);
+        //this.getCount(); retourne le nombre de tab
+        if(this.getCount()==1)
+        {
+            this.hideTabStripItem(0);
+        }
         
     },
     onTabAdd : function()
